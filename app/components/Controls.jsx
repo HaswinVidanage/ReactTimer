d2217a4f2be36d542c1e6a5c14c4c50a4db71491
Haswin Vidanage <Haswin Vidanage>
2017-04-14 09:46:11 +0530

    add Wire up controls component

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -1,23 +1,30 @@
 var React = require('react');
 
+
 var Controls = React.createClass({
   PropTypes :{
-    countdownStatus : React.PropTypes.string.isRequired
+    countdownStatus : React.PropTypes.string.isRequired,
+    onStatusChange : React.PropTypes.func.isRequired
+  },
+  onStatusChange : function(newStatus){
+    return () => {
+      this.props.onStatusChange(newStatus);
+    }
   },
   render : function(){
     var {countdownStatus} = this.props;
     var renderStartButton = () => {
       if(countdownStatus === 'started'){
-        return <button className="secondary button">Pause</button>
+        return <button className="secondary button" onClick={this.onStatusChange('paused')}>Pause</button>
       }else if (countdownStatus === 'paused'){
-        return <button className="primary button">Start</button>
+        return <button className="primary button" onClick={this.onStatusChange('started')}>Start</button>
       }
 
     };
     return (
       <div className="controls">
         {renderStartButton()}
-        <button className="alert hollow button">Clear</button>
+        <button className="alert hollow button" onClick={this.onStatusChange('stopped')}>Clear</button>
       </div>
     );
   }
